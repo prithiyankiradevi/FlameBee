fbb1cb3ec19e4a4bf753f19e95089260dc75aa69
Faizul <faizul.n@firebeetechnoservices.com>
2022-03-25 10:21:34 +0530

    second commit

--- config/url_config.js
+++ config/url_config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
-    //url:"mongodb+srv://Gowsi:Gowsi2898@cluster0.44gjt.mongodb.net/FlameBee?retryWrites=true&w=majority"
-       url:"mongodb://localhost:27017/FlameBee",
+    url:"mongodb+srv://Gowsi:Gowsi2898@cluster0.44gjt.mongodb.net/FlameBee?retryWrites=true&w=majority"
+      //url:"mongodb://localhost:27017/FlameBee",
     
 }
 
